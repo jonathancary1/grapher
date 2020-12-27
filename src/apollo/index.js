90aabe0e2ba92316fc81485fa578b693f4b37ee7
Jonathan Cary <jonathancary1@gmail.com>
2020-12-28 01:41:55 +0900

    Set grapher-api URI

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -3,6 +3,6 @@ import ApolloClient from 'apollo-boost';
 
 export default new VueApollo({
   defaultClient: new ApolloClient({
-    uri: 'http://localhost/api/graphql',
+    uri: 'https://grapher.caryjonathan.com/api/graphql',
   }),
 });
